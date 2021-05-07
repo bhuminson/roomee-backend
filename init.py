ae1df3f1234ed782c26ec74bfd01339a94f8d01d
bhuminson <bhumin0804@gmail.com>
2021-05-07 15:16:49 -0700

    delete unused code

--- init.py
+++ init.py
@@ -1,5 +1,4 @@
 import os
-from src import db_setup
 from src import auth
 from src import user
 from flask import Flask
@@ -15,27 +14,3 @@ app.register_blueprint(auth.bp)
 @app.route('/')
 def index():
     return "<h1>This is the backend for the Roomee app.</h1>"
-
-
-# def connect():
-#     """ Connect to the PostgreSQL database server """
-#     conn = None
-#     try:
-#         conn = psycopg2.connect(
-#             host="localhost",
-#             database="roomee",
-#             user="postgres",
-#             password=" ")
-#         cur = conn.cursor()
-
-#         cur.execute('SELECT * from users')
-#         cur.close()
-#     except (Exception, psycopg2.DatabaseError) as error:
-#         print(error)
-#     finally:
-#         if conn is not None:
-#             conn.close()
-#             print('Database connection closed.')
-
-
-# connect()

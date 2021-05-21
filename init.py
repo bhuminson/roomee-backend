2d4a58d309d488a2c4744fa215b62cac28f2bf99
bhuminson <bhumin0804@gmail.com>
2021-05-20 23:12:27 -0700

    delete unused code

--- init.py
+++ init.py
@@ -6,14 +6,10 @@ from flask_cors import CORS
 from flask_login import (
     LoginManager
 )
-import psycopg2
 
-app = Flask(__name__, static_folder="public",
-            instance_relative_config=True)
-CORS(app,
-    resources={r"*": {"origins": "http://localhost:3000"}},
-    expose_headers=["Content-Type", "x-csrftoken"],
-    supports_credentials=True,)
+app = Flask(__name__)
+CORS(app)
+
 app.config.update(
     DEBUG=True,
     SECRET_KEY="secret_sauce",
@@ -27,19 +23,21 @@ login_manager.init_app(app)
 login_manager.session_protection = "strong"
 login_manager.login_message = "Welcome to Roomee!"
 
-@login_manager.user_loader
-def load_user(id: int):
-    user_data = user.getUserProfile(id)
-    if user_data:
-        user_model = User()
-        user_model.id = user_data["id"]
-        return user_model
-    return None
+# Unused at the moment
+# @login_manager.user_loader
+# def load_user(id: int):
+#     user_data = user.getUserProfile(id)
+#     if user_data:
+#         user_model = User()
+#         user_model.id = user_data["id"]
+#         return user_model
+#     return None
+
 
 app.register_blueprint(user.bp)
-app.register_blueprint(auth.bp)
 app.register_blueprint(login.bp)
 app.register_blueprint(register.bp)
+
 
 @app.route('/')
 def index():

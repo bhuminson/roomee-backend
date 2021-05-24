a018add62759b95985aa5f51b57294a7b3ed274b
yuya <y303sushi@gmail.com>
2021-05-24 14:51:56 +0900

    tested with 'user' table

--- src/account_test.py
+++ src/account_test.py
@@ -11,47 +11,47 @@ def test_username():
 
 def test_firstname():
     result = executeQuery( 'SELECT firstname \
-                            FROM test_users \
-                            WHERE id = %s', [100]).get('firstname')
+                            FROM users \
+                            WHERE id = %s', [1]).get('firstname')
     assert result =='Draymond'
 
 
 def test_lastname():
     result = executeQuery('SELECT * \
-                            FROM test_users \
+                            FROM users \
                             WHERE \
-                            id = %s', [100]).get('lastname')
+                            id = %s', [1]).get('lastname')
     assert result =='Green'
 
 
 def test_nickname():
     result = executeQuery('SELECT nickname \
-                            FROM test_users \
+                            FROM users \
                             WHERE \
-                            id = %s', [100]).get('nickname')
+                            id = %s', [1]).get('nickname')
     assert result =='Day day'
 
 
 def test_phone():
     result = executeQuery('SELECT phone \
-                            FROM test_users \
+                            FROM users \
                             WHERE \
-                            id = %s', [100]).get('phone')
+                            id = %s', [1]).get('phone')
     assert result =='5105105100'
 
 
 def test_email():
     result = executeQuery('SELECT email \
-                            FROM test_users \
+                            FROM users \
                             WHERE \
-                            id =%s',[100]).get('email')
-    assert result =='q3@gmail.com'
+                            id =%s',[1]).get('email')
+    assert result =='dayday23@gmail.com'
     
 
 def test_bio():
     result = executeQuery('SELECT bio \
-                            FROM test_users \
+                            FROM users \
                             WHERE \
-                            id =%s',[100]).get('bio')
+                            id =%s',[1]).get('bio')
     assert result =='pf/c at gsw'
     

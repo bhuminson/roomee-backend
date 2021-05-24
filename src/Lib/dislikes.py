b4cca0d5136a3892346964e28373b2c38f33baf9
bhuminson <bhumin0804@gmail.com>
2021-05-23 18:31:58 -0700

    fix typos

--- src/Lib/dislikes.py
+++ src/Lib/dislikes.py
@@ -2,16 +2,16 @@ from src.db import executeQuery
 from src.constants import testing
 from psycopg2 import sql
 
-dislikesTable = "test_likes" if testing else "likes"
+dislikesTable = "test_dislikes" if testing else "dislikes"
 
 
 def dislikeRoomee(userId, roomee):
-    return executeQuery(sql.SQL("INSERT INTO {} (userId, likeId) VALUES(%s, %s)")
+    return executeQuery(sql.SQL("INSERT INTO dislikes (userId, likeId) VALUES(%s, %s)")
                         .format(sql.Identifier(dislikesTable)),
                         [userId, roomee], commit=True)
 
 
 def resetDislikes(userId):
-    executeQuery(sql.SQL('DELETE FROM {} WHERE userId=%s')
-                 .format(sql.Identifier(dislikesTable)),
-                 [userId], commit=True)
+    return executeQuery(sql.SQL('DELETE FROM {} WHERE userId=%s')
+                        .format(sql.Identifier(dislikesTable)),
+                        [userId], commit=True)

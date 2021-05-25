864b6cd5245116814b9bcfeab07a5adaa204f4dc
thaliaward <thalia.ward@yahoo.com>
2021-05-24 23:05:04 -0700

    added other rough test case

--- src/Lib/login.py
+++ src/Lib/login.py
@@ -5,11 +5,19 @@ from psycopg2 import sql
 usersTable = "test_users" if testing else "users"
 loginTable = "test_login_info" if testing else "login_info"
 
+# pretty sure the hashing may make this test case wrong
 @pytest.fixture(scope="function")
 def getPassword(username):
     hashed_pw = executeQuery(sql.SQL('SELECT password \
                                 FROM users \
                                 JOIN login_info on id=login_info.userId \
-                                WHERE username=%s').format(sql.Identifier(loginTable)),
+                                WHERE username=%s').format(sql.Identifier(usersTable), sql.Identifier(loginTable)),
                                 [username])
     return hashed_pw
+
+@pytest.fixture(scope="function")
+def getId(username):
+    return executeQuery(sql.SQL('SELECT id \
+                            FROM users \
+                            WHERE username=%s').format(sql.Identifier(usersTable)),
+                            [username])
--- src/Tests/login_test.py
+++ src/Tests/login_test.py
@@ -1,10 +1,16 @@
 import pytest
 from src.Lib.login import getPassword
+from src.Lib.login import getId
 from src.Tests.test_data import *
 
 def testGetPassword():
     result = getPassword('dayday23')
     assert result == 'dayday23'
 
+def testGetId():
+    result = getId('dayday23')
+    assert result == 1
+
 if __name__ == "__main__":
     testGetPassword()
+    testGetId()

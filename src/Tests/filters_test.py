def06ad0d2a2afb993e5971e6b5167825366a1d7
bhuminson <bhumin0804@gmail.com>
2021-05-26 15:17:35 -0700

    fix test

--- src/Tests/filters_test.py
+++ src/Tests/filters_test.py
@@ -3,5 +3,5 @@ from src.Tests.test_data import *
 
 
 def testFilters():
-    result = getNextMatchingRoomee(7, filter1)
+    result = getNextMatchingRoomee(1, filter1)
     assert result['username'] == user2['username']

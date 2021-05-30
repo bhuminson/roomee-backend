5fbeba0ffad32cb3fe1c69cb40b7ae95a634f102
thaliaward <thalia.ward@yahoo.com>
2021-05-29 17:32:17 -0700

    added negative login tests

--- src/Tests/login_test.py
+++ src/Tests/login_test.py
@@ -7,10 +7,20 @@ def testGetPassword():
     result = getPassword('dayday23')
     assert result == 'dayday23'
 
+def testWrongPassword():
+    result = getPassword('dayday23')
+    assert result != 'curry30'
+
 def testGetId():
     result = getId('dayday23')
     assert result == 1
 
+def testWrongId():
+    result = getId('dayday23')
+    assert result != 2
+
 if __name__ == "__main__":
     testGetPassword()
+    testWrongPassword()
     testGetId()
+    testWrongId()

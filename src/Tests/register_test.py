188599a7be6dc39a706b957cc9c2b652b6203823
bhuminson <bhumin0804@gmail.com>
2021-06-02 15:04:35 -0700

    correct register tests

--- src/Tests/register_test.py
+++ src/Tests/register_test.py
@@ -1,56 +1,67 @@
-from db import executeQuery
+from src.db import executeQuery
+from src.Tests.test_data import *
 
 
 def test_username():
 
     result = executeQuery('SELECT username \
-                            FROM users \
+                            FROM test_users \
                             WHERE id=%s ', [1]).get('username')
-    assert result == 'dayday23'
+    assert result == user1['username']
 
 
 def test_firstname():
     result = executeQuery('SELECT firstname \
-                            FROM users \
+                            FROM test_users \
                             WHERE id = %s', [1]).get('firstname')
-    assert result == 'Draymond'
+    assert result == user1['firstname']
 
 
 def test_lastname():
-    result = executeQuery('SELECT * \
-                            FROM users \
+    result = executeQuery('SELECT lastname \
+                            FROM test_users \
                             WHERE \
                             id = %s', [1]).get('lastname')
-    assert result == 'Green'
+    assert result == user1['lastname']
 
 
 def test_nickname():
     result = executeQuery('SELECT nickname \
-                            FROM users \
+                            FROM test_users \
                             WHERE \
                             id = %s', [1]).get('nickname')
-    assert result == 'Day day'
+    assert result == user1['nickname']
 
 
 def test_phone():
     result = executeQuery('SELECT phone \
-                            FROM users \
+                            FROM test_users \
                             WHERE \
                             id = %s', [1]).get('phone')
-    assert result == '5105105100'
+    assert result == user1['phone']
 
 
 def test_email():
     result = executeQuery('SELECT email \
-                            FROM users \
+                            FROM test_users \
                             WHERE \
                             id =%s', [1]).get('email')
-    assert result == 'dayday23@gmail.com'
+    assert result == user1['email']
 
 
 def test_bio():
     result = executeQuery('SELECT bio \
-                            FROM users \
+                            FROM test_users \
                             WHERE \
                             id =%s', [1]).get('bio')
-    assert result == 'pf/c at gsw'
+    assert result == user1['bio']
+
+
+def testRegister():
+    test_username()
+    test_firstname()
+    test_lastname()
+    test_nickname()
+    test_phone()
+    test_email()
+    test_bio()

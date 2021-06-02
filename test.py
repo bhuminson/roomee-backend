676c2c0b0b7414d280f4c5ae7b2e7fd69a48a057
bhuminson <bhumin0804@gmail.com>
2021-06-01 22:10:44 -0700

    set test flag

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from src.Tests.filters_test import testFilters
 from src.Tests.test_tables import testSetup
 from src.Tests.login_test import testLogin
+from src.constants import testing
+
+testing = True
 
 if __name__ == "__main__":
     testSetup()

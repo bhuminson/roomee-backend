64c9f478cabab29e1a579837e111952f644341cc
bhuminson <bhumin0804@gmail.com>
2021-06-04 00:59:44 -0700

    register methods

--- src/register.py
+++ src/register.py
@@ -6,24 +6,37 @@ from flask import (Blueprint, request, jsonify)
 bp = Blueprint('register', __name__)
 
 
-@bp.route('/register', methods=['POST'])
+@bp.route('/register', methods=['POST', 'PUT'])
 def register():
-    data = request.json
-    createNewUser(data)
-    createNewUserFilters(data)
-    resp = jsonify(success=True)
-    resp.status_code = 200
-    return resp
+    if request.method == "POST":
+        data = request.json
+        createNewUser(data)
+        createNewUserFilters(data)
+        createNewCredentials(data)
+        resp = jsonify(success=True)
+        resp.status_code = 201
+        return resp
+    elif request.method == "PUT":
+        data = request.json
+        updateUser(data)
+        updateFilters(data)
+        resp = jsonify(success=True)
+        resp.status_code = 200
+        return resp
 
 
-@bp.route('/image/<userId>', methods=['GET', 'POST'])
+@bp.route('/image/<userId>', methods=['GET', 'POST', 'PUT'])
 def image(userId):
     if request.method == "GET":
         return getUserImage(userId)
     elif request.method == "POST":
         image = request.files.get('img')
         img = image.read()
-        return uploadImage(img)
+        return uploadImage(img, userId)
+    elif request.method == "PUT":
+        image = request.files.get('img')
+        img = image.read()
+        return updateImage(img, userId)
 
 
 @bp.route('/resetImages', methods=['DELETE'])
